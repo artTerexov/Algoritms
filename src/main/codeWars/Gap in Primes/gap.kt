a858f62452829116d42ae55c022d95570d09c31a
artHouse <art133140@gmail.com>
2020-10-20 20:45:11 +0300

    some changes in Gap in Primes task

--- src/main/codeWars/Gap in Primes/gap.kt
+++ src/main/codeWars/Gap in Primes/gap.kt
@@ -1,7 +1,6 @@
 //if these g-gap prime numbers don't exist return []
 fun gap(g:Int, m:Long, n:Long):LongArray {
     var firstNum: Long
-    val array = LongArray(2)
     val list = arrayListOf<Long>()
 
     for (i in m..n) {
@@ -13,13 +12,11 @@ fun gap(g:Int, m:Long, n:Long):LongArray {
     for (i in 0 until list.lastIndex) {
         firstNum = list[i]
         if (list[i + 1] - firstNum == g.toLong()) {
-            array[0] = firstNum
-            array[1] = list[i + 1]
-            return array
+            return longArrayOf(firstNum, list[i + 1])
         }
     }
 
-    return LongArray(0)
+    return longArrayOf()
 }
 
 fun isPrime(m: Long): Boolean {
